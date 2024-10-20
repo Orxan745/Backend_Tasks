d47cd10528c4d245536520ec6a0b63096881eebb
Orxan745 <orxan8586orxan@gmail.com>
2024-10-20 21:23:26 +0400

    First commit!

---
+++ src/String_Methods_Char/Task1.java
@@ -0,0 +1,14 @@
+package String_Methods_Char;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.print("Zəhmət olmasa parolun uzunluğunu daxil edin: ");
+        int a = scanner.nextInt(); // a --> parolun uzunluğu
+
+    }
+}
