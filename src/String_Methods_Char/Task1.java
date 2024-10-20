03c55828d842de06051bd3ceed75789cc1b6ce40
Orxan745 <orxan8586orxan@gmail.com>
2024-10-20 21:49:54 +0400

    Second commit

--- src/String_Methods_Char/Task1.java
+++ src/String_Methods_Char/Task1.java
@@ -8,7 +8,16 @@ public class Task1 {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         System.out.print("Zəhmət olmasa parolun uzunluğunu daxil edin: ");
-        int a = scanner.nextInt(); // a --> parolun uzunluğu
-
+        int a = scanner.nextInt();        // a --> parolun uzunluğu
+        int b;        // Random seçilib char-a castlanacaq dəyişən
+        char c;      // Parolun elementləri olacaq dəyişən
+        String result = ""; // parol
+        for (int i = 0; i < a; i++){
+            //Istifadəçi tərəfindən təyin edilən a qədər random ədəd almaq üçün dövr
+            b = random.nextInt(48,122);
+            c = (char) b;
+            result = result + c;
+        }
+        System.out.println(result);
     }
 }
