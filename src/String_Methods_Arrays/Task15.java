0b7009401e2e7e9edcc92b460a369636318c6a3f
Orxan745 <orxan8586orxan@gmail.com>
2024-10-25 10:19:04 +0400

    Commit

---
+++ src/String_Methods_Arrays/Task15.java
@@ -0,0 +1,26 @@
+package String_Methods_Arrays;
+
+import java.util.Scanner;
+
+public class Task15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Zəhmət olmasa mətn daxil edin: ");
+        String mtn = scanner.nextLine();
+        FirstLetterUpperCase(mtn);
+    }
+
+    public static void FirstLetterUpperCase(String a) {
+        String[] str = a.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            String x = str[i].substring(0, 1);
+            String y = x.toUpperCase();
+            str[i] = str[i].replaceFirst(x, y);
+        }
+        String result = "";
+        for (int i = 0; i < str.length; i++) {
+            result = result + str[i] + " ";
+        }
+        System.out.println(result);
+    }
+}
---
+++ src/String_Methods_Arrays/Task16.java
@@ -0,0 +1,14 @@
+package String_Methods_Arrays;
+
+import java.util.Scanner;
+import String_Methods_Char.Task8;
+
+public class Task16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+    public static void All_Combination(String a) {
+        int count = Task8.Faktorial(a.length());
+    }
+}
